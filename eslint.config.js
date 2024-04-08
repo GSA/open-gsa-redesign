27b49e9a250105ef19321d3ac5526ac1477d41d6
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2024-04-08 10:41:18 -0600

    update eslint config to work with v9

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
